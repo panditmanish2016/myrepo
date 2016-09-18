095509ab56238730fba9ecb4358ac13a71e5366b
manish kumar <manish kumar>
2016-09-18 15:59:45 +0530

    added from remotebranch

--- Test.java
+++ Test.java
@@ -3,5 +3,7 @@ class Test
 	System.out.println("this is from master branch");
 	System.out.println("this is from master branch again");
 	System.out.println("this is from remote branch again");
-	System.out.println("this is from  localbranch ");	
+	System.out.println("this is from  localbranch ");
+	System.out.println("this is from  remotebranch ");
+	System.out.println("this is from  remotebranch1 ");
 }
