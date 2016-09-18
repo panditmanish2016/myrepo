74a25497a063280ad5f38b247be1caebbea22248
manish kumar <panditmanish680@gmail.com>
2016-09-18 15:40:01 +0530

    Update Test.java
    added line of code from remote branch

--- Test.java
+++ Test.java
@@ -2,5 +2,5 @@ class Test
 {
 	System.out.println("this is from master branch");
 	System.out.println("this is from master branch again");
-	
+	System.out.println("this is from remote branch again");
 }
