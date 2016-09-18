b78af2605fe17ce2bf04b05d72eec7f62744f2d7
manish kumar <manish kumar>
2016-09-18 15:51:23 +0530

    added by localbranch

--- Test.java
+++ Test.java
@@ -3,4 +3,5 @@ class Test
 	System.out.println("this is from master branch");
 	System.out.println("this is from master branch again");
 	System.out.println("this is from remote branch again");
+	System.out.println("this is from  localbranch ");	
 }
